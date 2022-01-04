38e4ab403f109bdc57ec89d72fc08875215e0934
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2022-01-04 09:23:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -212,7 +212,7 @@ async def delete_all_index(bot, message):
 
 @Client.on_callback_query()
 async def cb_data(bot, update):
-    if message.data == "owner":
+    if update.data == "owner":
         await query.answer("✨...ʙᴏᴛ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟs...✨\n\n😶‍🌫️ғᴜʟʟ\nɴᴀᴍᴇ :₲Ø₭Ʉ ₴Ø₦\n🍻ᴜsᴇʀɴᴀᴍᴇ: @ivar_bonel\n\n🍒ᴄᴏᴜɴᴛʀʏ: I N D I A\n🐾sᴛᴀᴛᴇ: K E R A L A\n\n{query.from_user.first_name} സാർ എന്നാൽ ഞാൻ അങ്ങോട്ട് പൊയ്ക്കോട്ടെ🤗", show_alert=True)
 
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
